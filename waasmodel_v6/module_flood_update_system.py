e89786cdc6c193dc207327adc1df72f097533e71
JuliusSchlumberger <j.schlumberger@posteo.de>
2024-05-01 12:37:52 +0200

    Waasmodel and additional files for output creation & change pathways sequencing changes if measure removed (keep &&)

--- waasmodel_v6/module_flood_update_system.py
+++ waasmodel_v6/module_flood_update_system.py
@@ -15,22 +15,21 @@ class UpdateGeneralsystem:
     def run_module(self, timestep, module_transfer, inputs):
         # urb_growth = self.initiate_timeinputs(timestep, inputs)
         urb_growth = 30 # grows 30 cells (100 by 100 m) at timestep when called
-
+        print('urban area grew.')
         module_transfer.lost_cropland += np.maximum(0, urb_growth)
-        landuse, agri_areas, no_constr = urban_growth(growth=urb_growth, LUmap=module_transfer.LandUse, no_constr=module_transfer.no_constr)
+        landuse, agri_areas = urban_growth(growth=urb_growth, LUmap=module_transfer.LandUse)
         expos = lookupscalar(inputs.MaxDamTbl, landuse)  # max damage per LU
         # derive total agricultural area in the Waasregion
         agri_areas = maptotal(ifthen(landuse == 9, scalar(1)))  # in [ha]!
         agri_areas = pcr2numpy(agri_areas, -999)[0, 0]
-        self.create_output_dicts(landuse, expos, agri_areas, no_constr, module_transfer)
+        self.create_output_dicts(landuse, expos, agri_areas, module_transfer)
         return module_transfer
 
     def initiate_timeinputs(self, timestep, inputs):
         urb_growth = inputs.urb_scen[int(timestep / 36 / 20 - 1), self.urb_scenario]
         return urb_growth
 
-    def create_output_dicts(self, landuse, expos, agri_areas, no_constr, module_transfer):
+    def create_output_dicts(self, landuse, expos, agri_areas, module_transfer):
         module_transfer.LandUse = landuse
-        module_transfer.no_constr = no_constr
         module_transfer.expos = expos
         module_transfer.agri_areas = agri_areas
